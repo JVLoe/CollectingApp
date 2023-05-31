fe214fca094e197b5f71c78372a81c7a11dfd525
JVLoe <108595341+JVLoe@users.noreply.github.com>
2023-05-31 13:04:49 +0100

    new php collecting app

---
+++ myplanets_export_2023-05-05_184735.sql
@@ -0,0 +1,4 @@
+insert into `myplanets` (`colour`, `id`, `name`, `size`, `temperature`) values ('Yellow-White', 1, 'Venus', '6051.8 km', '475C');
+insert into `myplanets` (`colour`, `id`, `name`, `size`, `temperature`) values ('Red', 2, 'Mars', '3389.5 km', '-65C');
+insert into `myplanets` (`colour`, `id`, `name`, `size`, `temperature`) values ('Dusty-Orange', 3, 'Jupiter', '69911 km', '-110C');
+insert into `myplanets` (`colour`, `id`, `name`, `size`, `temperature`) values ('Azure-Blue', 4, 'Neptune', '24622 km', '-225C');
